309bb54de46e7d09774bd954d12bafa6e3958240
YoungH66 <rladudgns2222@gmail.com>
2024-11-22 09:09:57 +0900

    ✨ Feat : 기본 칵테일 관리 기능 누락된 클래스 추가
    basecocktail를 초기화하는 과정에서 누락되었던 클래스를 추가했습니다.
    - domain에 클래스가 추가되었습니다.
      - CocktailCreate.java
      - CocktailSearch.java
    - web/request와 web/reponse에 클래스가 추가되었습니다.
      - CocktailSearchRequest.java
      - CocktailDetailResponse.java
    
    Ref : #9

---
+++ src/main/java/org/toastit_v2/feature/basecocktail/web/request/CocktailSearchRequest.java
@@ -0,0 +1,4 @@
+package org.toastit_v2.feature.basecocktail.web.request;
+
+public class CocktailSearchRequest {
+}
---
+++ src/main/java/org/toastit_v2/feature/basecocktail/web/response/CocktailDetailResponse.java
@@ -0,0 +1,4 @@
+package org.toastit_v2.feature.basecocktail.web.response;
+
+public class CocktailDetailResponse {
+}
