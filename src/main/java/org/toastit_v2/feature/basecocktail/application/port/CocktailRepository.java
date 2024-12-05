2b16a1038e9cbbe59b2ba306c9b050fab7356467
YoungH66 <rladudgns2222@gmail.com>
2024-12-06 05:17:41 +0900

    ✨ Feat : 기본 칵테일 조회 포트 추가
    기본 칵테일 조회에 사용될 포트를 정의
      - 도메인 객체만을 다루는 포트를 추가함.
      - 현 상황에서 사용될만한 기능만 정의. 추후 추가 가능.
    
    Ref : #9

--- src/main/java/org/toastit_v2/feature/basecocktail/application/port/CocktailRepository.java
+++ src/main/java/org/toastit_v2/feature/basecocktail/application/port/CocktailRepository.java
@@ -1,4 +1,22 @@
 package org.toastit_v2.feature.basecocktail.application.port;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.toastit_v2.feature.basecocktail.domain.Cocktail;
+import org.toastit_v2.feature.basecocktail.domain.CocktailSearch;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CocktailRepository {
+    // 검색 관련
+    Page<Cocktail> search(CocktailSearch searchCondition, Pageable pageable);
+
+    // ID 기반 조회
+    Optional<Cocktail> findById(ObjectId id);
+    List<Cocktail> findByIdIn(List<ObjectId> ids);
+
+    // 랜덤 조회
+    List<Cocktail> findRandom(int count);
 }
