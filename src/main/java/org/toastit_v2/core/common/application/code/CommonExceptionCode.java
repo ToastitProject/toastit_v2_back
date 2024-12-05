65c30537fe1b63a3076afb96550e9fa99758080e
YoungH66 <rladudgns2222@gmail.com>
2024-12-06 06:57:37 +0900

    ✨ Feat : 기본 칵테일 조회 에러코드 추가
    기본 칵테일 조회 기능을 위한 에러코드 추가
      - 랜덤한 칵테일을 반환하는 기능을 위한 에러코드 추가
      - INVALID_COCKTAIL_SEARCH의 data를 포괄적으로 변경
        - 에러코드의 data는 차후에 변경될 수 있음.
    
    Ref : #9

--- src/main/java/org/toastit_v2/core/common/application/code/CommonExceptionCode.java
+++ src/main/java/org/toastit_v2/core/common/application/code/CommonExceptionCode.java
@@ -77,7 +77,9 @@ public enum CommonExceptionCode implements ResponseCode {
     // 칵테일을 찾을 수 없음
     NOT_FOUND_COCKTAIL(HttpStatus.NOT_FOUND, "칵테일을 찾을 수 없습니다."),
     // 검색 조건이 올바르지 않음
-    INVALID_COCKTAIL_SEARCH(HttpStatus.BAD_REQUEST, "잘못된 칵테일 검색 조건입니다."),
+    INVALID_COCKTAIL_SEARCH(HttpStatus.BAD_REQUEST, "검색 조건이 올바르지 않습니다."),
+    // 칵테일 개수가 올바르지 않음
+    INVALID_COCKTAIL_COUNT(HttpStatus.BAD_REQUEST, "유효하지 않은 칵테일 개수입니다."),
 
     /**
      * Like Error
