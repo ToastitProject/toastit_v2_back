59dc37e24aa18602e6e08f5e2258ac57ce34d47f
YoungH66 <rladudgns2222@gmail.com>
2024-12-06 07:27:09 +0900

    ✨ Feat : 기본 칵테일 조회 인프라스트럭쳐 구현
    누락된 부분 추가
     - 구현된 CocktailRepositoryImpl 추가
    
    Ref : #9

--- src/main/java/org/toastit_v2/feature/basecocktail/infrastructure/persistence/mongodb/CocktailRepositoryImpl.java
+++ src/main/java/org/toastit_v2/feature/basecocktail/infrastructure/persistence/mongodb/CocktailRepositoryImpl.java
@@ -1,4 +1,49 @@
 package org.toastit_v2.feature.basecocktail.infrastructure.persistence.mongodb;
 
-public class CocktailRepositoryImpl {
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.toastit_v2.feature.basecocktail.application.port.CocktailRepository;
+import org.toastit_v2.feature.basecocktail.domain.Cocktail;
+import org.toastit_v2.feature.basecocktail.domain.CocktailSearch;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class CocktailRepositoryImpl implements CocktailRepository {
+
+    private final MongoCocktailRepository mongoRepository;
+
+    @Override
+    public Page<Cocktail> search(CocktailSearch searchCondition, Pageable pageable) {
+        return mongoRepository.search(searchCondition, pageable)
+                .map(CocktailDocument::toDomain);
+    }
+
+    @Override
+    public Optional<Cocktail> findById(ObjectId id) {
+        return mongoRepository.findById(id)
+                .map(CocktailDocument::toDomain);
+    }
+
+    @Override
+    public List<Cocktail> findByIdIn(List<ObjectId> ids) {
+        return mongoRepository.findAllById(ids)
+                .stream()
+                .map(CocktailDocument::toDomain)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Cocktail> findRandom(int count) {
+        return mongoRepository.findRandom(count)
+                .stream()
+                .map(CocktailDocument::toDomain)
+                .collect(Collectors.toList());
+    }
 }
