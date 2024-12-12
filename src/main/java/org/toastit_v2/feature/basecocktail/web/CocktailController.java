1e07e75009996e18f9c1844f1b06c8108480d307
YoungH66 <rladudgns2222@gmail.com>
2024-12-13 07:06:02 +0900

    ♻️ 리팩토링 : CocktailController의 구조를 수정
    Controller의 구조를 RequestParam을 사용하도록 변경.
      - 차후 위 방식을 생성자 패턴을 사용해서 객체가 탄생할때 제약조건을 준수하도록 변경할 예정.
    
    Ref : #9

--- src/main/java/org/toastit_v2/feature/basecocktail/web/CocktailController.java
+++ src/main/java/org/toastit_v2/feature/basecocktail/web/CocktailController.java
@@ -5,12 +5,11 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import org.toastit_v2.core.common.application.code.CommonExceptionCode;
 import org.toastit_v2.core.common.application.config.swagger.ExceptionCodeAnnotations;
 import org.toastit_v2.feature.basecocktail.application.service.CocktailService;
-import org.toastit_v2.feature.basecocktail.domain.Cocktail;
 import org.toastit_v2.feature.basecocktail.web.response.CocktailResponse;
 
 import java.util.List;
@@ -33,11 +32,12 @@ public class CocktailController {
     })
     @GetMapping("/search")
     public Page<CocktailResponse> search(
-            @Parameter(description = "검색 키워드 (예: '알코올 레몬', '진, 라임')")
+            @Parameter(description = "검색 키워드", example = "알코올 레몬")
             @RequestParam String keyword,
-            Pageable pageable
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "20") int size
     ) {
-        return cocktailService.search(keyword, pageable)
+        return cocktailService.search(keyword, PageRequest.of(page, size))
                 .map(CocktailResponse::from);
     }
 
