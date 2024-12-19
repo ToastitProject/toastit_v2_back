ca19159cee3d2028d7a199ae3de339f06da72561
YoungH66 <rladudgns2222@gmail.com>
2024-12-20 07:58:37 +0900

    🐛 버그 수정 : mongodb 빈 충돌 현상 수정
    mongodb를 사용하는데 있어서 발생하는 빈 충돌 이슈가 있습니다.
    지금까지 yml파일에 명시적으로 빈 중복을 허용했지만, 해당 조치는 이후 문제발생 가능성이 높기 때문에 삭제합니다.
    빈 중복 이슈에 대해서는 MongoCocktailRepository.java에 @ExcludeFromJpaRepositories 어노테이션을 통해서 jpa 리포지토리로 간주되지 않게 처리하는 것으로 조치했습니다.
    
    Ref : #9

--- src/main/java/org/toastit_v2/feature/basecocktail/infrastructure/persistence/mongodb/MongoCocktailRepository.java
+++ src/main/java/org/toastit_v2/feature/basecocktail/infrastructure/persistence/mongodb/MongoCocktailRepository.java
@@ -2,7 +2,9 @@ package org.toastit_v2.feature.basecocktail.infrastructure.persistence.mongodb;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.toastit_v2.core.common.application.config.querydsl.ExcludeFromJpaRepositories;
 import org.toastit_v2.feature.basecocktail.infrastructure.persistence.mongodb.custom.CustomCocktailRepository;
 
+@ExcludeFromJpaRepositories
 public interface MongoCocktailRepository extends MongoRepository<CocktailDocument, ObjectId>, CustomCocktailRepository {
 }
