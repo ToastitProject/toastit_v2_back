e70d52ad1c748f31053ba1b8637212c388ef6145
kang <kkace22@gmail.com>
2025-01-16 00:42:52 +0900

    ✨  Feat: Google Trend API 요청기능 추가 Google Trend API 요청문을 전송할 수 있는 메서드를 추가, 기존 코드 리펙토링 Ref : #23, #25

--- src/main/java/org/toastit_v2/feature/trendcocktail/application/service/TrendCocktailService.java
+++ src/main/java/org/toastit_v2/feature/trendcocktail/application/service/TrendCocktailService.java
@@ -1,89 +1,53 @@
 package org.toastit_v2.feature.trendcocktail.application.service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.toastit_v2.feature.trendcocktail.application.port.TrendCocktailRepository;
-import org.toastit_v2.feature.trendcocktail.application.service.utilly.dateService;
+import org.toastit_v2.feature.trendcocktail.application.service.utilly.GoogleAPIRequestMaker;
+import org.toastit_v2.feature.trendcocktail.application.service.utilly.NaverAPIRequestMaker;
 import org.toastit_v2.feature.trendcocktail.infrastructure.persistence.mysql.custom.JPATrendCocktailRepository;
-
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class TrendCocktailService implements TrendCocktailRepository {
 
     private final JPATrendCocktailRepository jpaRepository;
+    private final GoogleAPIRequestMaker googleAPIRequestMaker;
+    private final NaverAPIRequestMaker naverAPIRequestMaker;
 
-    private final RestTemplate restTemplate;
-
-    @Value("${api.naver.client-id}")
-    private String clientId;
-
-    @Value("${api.naver.client-secret}")
-    private String clientSecret;
-
-    public TrendCocktailService(JPATrendCocktailRepository jpaRepository, RestTemplate restTemplate) {
+    public TrendCocktailService(JPATrendCocktailRepository jpaRepository, GoogleAPIRequestMaker googleAPIRequestMaker, NaverAPIRequestMaker naverAPIRequestMaker) {
         this.jpaRepository = jpaRepository;
-        this.restTemplate = restTemplate;
+        this.googleAPIRequestMaker = googleAPIRequestMaker;
+        this.naverAPIRequestMaker = naverAPIRequestMaker;
     }
 
+    /**
+     * String 타입의 List 를 DB 에 저장하는 메서드 입니다.
+     * API 를 통해 얻어온 칵테일을 List 자료 구조에 저장하고 이를 DB에 저장할 때 사용합니다.
+     * @param cocktailList
+     */
     @Override
     public void save(List<String> cocktailList) {
         jpaRepository.save(cocktailList);
     }
 
+    /**
+     * Naver DataLaB 에 키워드들의 검색량을 응답받는 요청문을 전송하는 메서드 입니다.
+     * @param keywords : Project 에서 제공하는 칵테일 리스트를 입력받을 수 있습니다.
+     * @return 요청문을 반환 합니다.
+     */
     @Override
-    public String naverRequest(List<String> keywords) {
-        String url = "https://openapi.naver.com/v1/datalab/search";
-        HttpHeaders headers = createHeaders();
-        StringBuilder finalResponse = new StringBuilder();
-        for (int i = 0; i < keywords.size(); i += 5) {
-            List<String> sublist = keywords.subList(i, Math.min(i + 5, keywords.size()));
-            String requestBody = createRequestBody(sublist);
-            ResponseEntity<String> response = sendRequest(url, headers, requestBody);
-            finalResponse.append(response.getBody());
-            if (i + 5 < keywords.size()) {
-                finalResponse.append("||");
-            }
-        }
-        return finalResponse.toString();
+    public String naverTrendAPIRequest(List<String> keywords) {
+        return naverAPIRequestMaker.createNaverTrendAPIRequest(keywords);
     }
 
-    private HttpHeaders createHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-Naver-Client-Id", clientId);
-        headers.set("X-Naver-Client-Secret", clientSecret);
-        headers.set("Content-Type", "application/json");
-        return headers;
-    }
-
-    private String createRequestBody(List<String> sublist) {
-        StringBuilder keywordGroupBuilder = new StringBuilder();
-        keywordGroupBuilder.append("\"keywordGroups\":[");
-
-        for (int j = 0; j < sublist.size(); j++) {
-            keywordGroupBuilder.append(createKeywordGroup(sublist.get(j)));
-            if (j < sublist.size() - 1) {
-                keywordGroupBuilder.append(",");
-            }
-        }
-        keywordGroupBuilder.append("]");
-        return String.format("{\"startDate\":\"%s\",\"endDate\":\"%s\",\"timeUnit\":\"month\",%s}",
-                dateService.firstDayOfTwoMonthsAgo(),
-                dateService.lastDayOfLastMonth(),
-                keywordGroupBuilder.toString());
-    }
-
-    private String createKeywordGroup(String keyword) {
-        return String.format("{\"groupName\":\"%s\",\"keywords\":[\"%s\"]}", keyword, keyword);
-    }
-
-    private ResponseEntity<String> sendRequest(String url, HttpHeaders headers, String requestBody) {
-        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
-        return restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+    /**
+     * Google Trend API 를 통해 특정 키워드의 검색량을 응답받는 요청문을 전송하는 메서드 입니다.
+     * @param keywords : 검색량을 응답받고 싶은 키워드를 입력합니다.
+     * @return : 요청문을 반환합니다.
+     */
+    @Override
+    public Map<String,Object> googleTrendAPIRequest(String keywords) {
+       return googleAPIRequestMaker.createGoogleTrendAPIRequest(keywords);
     }
 }
