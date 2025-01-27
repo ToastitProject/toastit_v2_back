422b9e677be216130c0f314afa95e6dc1d9390ed
CodeJesterND <dev.hyoseung@gmail.com>
2025-01-28 01:45:30 +0900

    ♻️ 코드 리팩토링 : Random 객체를 static final로 상수화하여 객체 재사용을 통한 성능 최적화

--- src/main/java/org/toastit_v2/common/generator/auth/mail/MailKeyGeneratorImpl.java
+++ src/main/java/org/toastit_v2/common/generator/auth/mail/MailKeyGeneratorImpl.java
@@ -4,18 +4,19 @@ import java.util.Random;
 
 public class MailKeyGeneratorImpl implements MailKeyGenerator {
 
+    private static final Random RANDOM = new Random();
+
     @Override
     public String generate() {
         StringBuilder key = new StringBuilder();
-        Random rand = new Random();
 
         for (int i = 0; i < 8; i++) {
-            int index = rand.nextInt(3); // 0~2 까지 랜덤
+            int index = RANDOM.nextInt(3); // 0~2 까지 랜덤
 
             switch (index) {
-                case 0 -> key.append((char) (rand.nextInt(26) + 97));
-                case 1 -> key.append((char) (rand.nextInt(26) + 65));
-                case 2 -> key.append((rand.nextInt(10)));
+                case 0 -> key.append((char) (RANDOM.nextInt(26) + 97));
+                case 1 -> key.append((char) (RANDOM.nextInt(26) + 65));
+                case 2 -> key.append((RANDOM.nextInt(10)));
             }
         }
 
--- src/main/java/org/toastit_v2/feature/user/domain/UserStatus.java
+++
@@ -1,8 +0,0 @@
-package org.toastit_v2.feature.user.domain;
-
-public enum UserStatus {
-
-    INACTIVE,
-    ACTIVE
-
-}
