320150b5c61979d977b4fba596272e6d65593527
rowing0328 <dev.hyoseung@gmail.com>
2025-02-03 15:02:18 +0900

    ✅ 테스트 추가 : 인증 메일 도메인 테스트 추가 - AuthMail 객체 생성 시 올바른 이메일, 인증 번호, 등록 날짜가 설정되는지 검증
    Ref : #35

--- src/main/java/org/toastit_v2/core/domain/auth/mail/AuthMail.java
+++ src/main/java/org/toastit_v2/core/domain/auth/mail/AuthMail.java
@@ -1,13 +1,10 @@
 package org.toastit_v2.core.domain.auth.mail;
 
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.toastit_v2.common.exception.custom.CustomAuthMailException;
-import org.toastit_v2.common.generator.auth.mail.MailKeyGenerator;
+import org.toastit_v2.common.generator.auth.mail.MailAuthCodeGenerator;
 import org.toastit_v2.common.generator.date.DateTimeGenerator;
 import org.toastit_v2.common.response.code.ExceptionCode;
 
@@ -25,26 +22,27 @@ public class AuthMail {
     @Id
     private String userEmail;
 
-    private String authNumber;
+    private String authCode;
 
     private LocalDateTime registerDate;
 
-    private AuthMail(final String userEmail, final String authNumber, final LocalDateTime registerDate) {
+    @Builder
+    private AuthMail(final String userEmail, final String authCode, final LocalDateTime registerDate) {
         this.userEmail = userEmail;
-        this.authNumber = authNumber;
+        this.authCode = authCode;
         this.registerDate = registerDate;
     }
 
-    public static AuthMail create(final String userEmail, MailKeyGenerator mailKeyGenerator, DateTimeGenerator dateTimeGenerator) {
+    public static AuthMail create(final String userEmail, MailAuthCodeGenerator mailAuthCodeGenerator, DateTimeGenerator dateTimeGenerator) {
         return new AuthMail(
                 userEmail,
-                mailKeyGenerator.generate(),
+                mailAuthCodeGenerator.generate(),
                 dateTimeGenerator.generate()
         );
     }
 
     public void checkAuthNumber(final String authNumber) {
-        if (!authNumber.equals(this.authNumber)) {
+        if (!authNumber.equals(this.authCode)) {
             throw new CustomAuthMailException(ExceptionCode.AUTH_EMAIL_AUTH_NUMBER_ERROR);
         }
     }
